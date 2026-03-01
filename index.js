85e49f47aa78846a61fa629b7215116d46b29f16
GOPIINATH  R <72294974+GOPINATHRB@users.noreply.github.com>
2026-03-01 18:05:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
 
   if (req.method === "OPTIONS") {
     console.log("⚡ OPTIONS request handled");
-    return res.sendStatus(204); // Preflight response
+    return res.sendStatus(200); // Preflight response
   }
 
   next();
